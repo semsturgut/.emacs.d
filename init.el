8bf5eb99ad3af6a82ab251955cf0abc826cb7919
Şems Turgut <sems.turgut@parasut.com>
2020-06-14 13:46:35 +0300

    Cleaning init file for future updates.

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+;; ---===================---
+;; ----Package  Settings----
+;; ---===================---
+
 ;; Melpa Package
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
@@ -6,7 +10,11 @@
 (require 'use-package)
 ;; Start server
 (server-start)
-;; Visual Settings
+
+;; ---===================---
+;; -----Visual Settings-----
+;; ---===================---
+
 (set-frame-font "Source Code Pro 16" nil t)
 ;; Wrap toggle off
 (setq-default truncate-lines t)
@@ -14,21 +22,62 @@
 ;; (global-visual-line-mode t)
 ;; macOS frame issue solver  
 (setq frame-resize-pixelwise t)
+;; Show parentheses
 (show-paren-mode 1)
+(setq show-paren-delay 0)
+;; Close scroll bar
 (scroll-bar-mode -1)
+;; Close tool bar
 (tool-bar-mode -1)
+;; Auto brackets
 (electric-pair-mode 1)
-(menu-bar-mode -1)
 (setq electric-pair-preserve-balance nil)
+;; Toggle menu bar off
+(menu-bar-mode -1)
+;; Hide welcome screen
 (setq inhibit-startup-screen t)
+;; Save last session
+(desktop-save-mode 1)
+;; Prevent backup files from being created
+(setq make-backup-files nil)
+;; Show Line Numbers
+(global-linum-mode 1)
+(setq linum-format "%4d \u2502 ")
+
+;; ---===================---
+;; ---Additional Packages---
+;; ---===================---
+
 ;; Enable Multiple-cursors features
 (require 'multiple-cursors)
+;; helm mode
+(helm-mode 1)
+;; Show recent files
+(recentf-mode 1)
+;; Edit files by root
+(require 'sudo-edit)
+;; spaceline mode
+(require 'spaceline-config)
+;; Show bracket colors
+(require 'rainbow-delimiters)
+
+;; ---===================---
+;; ---Additional  Configs---
+;; ---===================---
+
+;; Fix for bash configuration
+;; Uncomment only on Linux
+;; (setq-default shell-file-name "/bin/bash")
+;; Uncomment only on macOS
+(exec-path-from-shell-copy-env "IDF_PATH")
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+;; Enable Multiple-cursors features
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 ;; helm mode
-(helm-mode 1)
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
@@ -39,42 +88,18 @@
 ;; delete file
 (global-set-key (kbd "C-c x c") #'delete-file)
 ;; Show recent files
-(recentf-mode 1)
 (setq recentf-max-menu-items 25)
 (global-set-key "\C-x\ \C-r" 'recentf-open-files)
 ;; Edit files by root
-(require 'sudo-edit)
 (global-set-key (kbd "C-c C-r") 'sudo-edit)
-;; Save last session
-(desktop-save-mode 1)
-;; Prevent backup files from being created
-(setq make-backup-files nil)
 ;; spaceline mode
-(require 'spaceline-config)
 (spaceline-spacemacs-theme)
-;; Fix for bash configuration
-;; Uncomment only on Linux
-;; (setq-default shell-file-name "/bin/bash")
-;; Uncomment only on macOS
 (setq powerline-image-apple-rgb t)
-(exec-path-from-shell-copy-env "IDF_PATH")
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
-;; Show Line Numbers
-(global-linum-mode 1)
-(setq linum-format "%4d \u2502 ")
 ;; Show bracket colors
-(require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-;; Delete selection
-(delete-selection-mode 1)
 
 ;; ---===================---
-;; ---Additional Packages---
-;; ---===================---
-
-;; ---===================---
-;; ---End---
+;; -----------End-----------
 ;; ---===================---
 
 (custom-set-faces
@@ -82,7 +107,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(cursor ((t (:background "SlateBlue1"))))
+ '(region ((t (:background "alternatingContentBackgroundColor" :foreground "SlateBlue1"))))
+ '(show-paren-match ((t (:background "DarkOrchid3")))))
 (put 'magit-clean 'disabled nil)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
